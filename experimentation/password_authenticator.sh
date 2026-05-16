648a0b7c48b06d9f3e15c98b447e504bbf0561e2
will presotn <williampreston2010@gmail.com>
2026-05-16 10:17:47 +0100

    still idk

--- experimentation/password_authenticator.sh
+++ experimentation/password_authenticator.sh
@@ -8,6 +8,7 @@ read -p "new account or sign in (n/s): " request
 if [ "$request" = "n" ]; then
 	read -p "new username: " new_username
 	touch ~/accounts/"$new_username"-credentials
+
 	while [ "$password_1" != "$password_2" ]; do
 		read -p "new password: " password_1
 		read -p "repeat password: " password_2
@@ -15,11 +16,13 @@ if [ "$request" = "n" ]; then
 			echo "passwords did not match"
 		fi
 	done
+
 	echo "$password_1" \
-        | argon2 7d6e1fc827f47307c27953bc83448eaf \
-        -id -t 5 -k 300000 -p 1 \
-        | grep "Hash" | sed -n 's/.*Hash://p' \
-	> ~/accounts/"$new_username"-credentials
+        	| argon2 7d6e1fc827f47307c27953bc83448eaf \
+        	-id -t 5 -k 300000 -p 1 \
+        	| grep "Hash" | sed -n 's/.*Hash://p' \
+		> ~/accounts/"$new_username"-credentials
+
 elif [ "$request" = "s" ]; then
 	read -p "account: " username
 	read -p "password: " password
--- networking/node-setup.sh
+++ networking/node-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-until ip_address="$(curl ifconfig.me)" > /dev/null 2>&1; do
+until ip_address="$(curl ifconfig.me)"; do
 	sleep 1
 done
 
@@ -9,7 +9,7 @@ sudo systemctl start tailscaled
 if [ "$ip_address" = "81.100.7.198" ]; then
 	tailscale set --exit-node=
 	notify-send "exit node inactive"
-else                              
+else
 	toggle-node-script.sh &
 fi
 
